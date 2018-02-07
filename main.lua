9ae2afac55bf129b6dd33a45860016e3567fe8b4
polispa <33254198+polispa@users.noreply.github.com>
2018-02-07 21:32:45 +0100

    Message de time out
    Retiré punition, message de time out intégré, gros lavage de bugs, code un peu plus propre, musique placeholder intégré.

--- main.lua
+++ main.lua
@@ -5,22 +5,25 @@ function love.load()
   score = 0
   hiScore = 0
   newHiScore = false --High score beaten?
-  posx = love.math.random(100, 550)
+  posx = love.math.random(100, 550) -- instructions position
   posy = love.math.random(100, 350)
+  --arrowPosx = love.math.random(100, 550) -- arrows positions
+  --arrowPosy = love.math.random(100, 350)
+  --angleArrows = {0, 1.57, 3.14, 4.71}
+  --a = 1
   angle = 0 -- instruction rotation
-  punishment = true
-  timeStart = 120 --timer setting
+  timeStart = 5 --timer setting
   time = timeStart
   mainMenu = true --player in main menu
   fail = false -- player mistake
-  --mos = love.graphics.newImage("image/38234.jpg")
+  timeOut = false --time out
 
   love.graphics.setNewFont(30)
   love.graphics.setBackgroundColor(0, 220, 255)
 
-  --abc = love.audio.newSource("sound/123.mp3", "stream")
+  percival = love.audio.newSource("sound/percival.mp3", "stream")
 
-  --love.audio.play(abc)
+  love.audio.play(percival) -- Play BGM
 end
 --------------------------------------------------------------
 function love.keyboard.setKeyRepeat(enable) -- avoids button spamming
@@ -35,15 +38,14 @@ function love.update(dt)
     love.event.quit()
   end
 
-  if mainMenu == false or fail == false then -- countdown starts only in-game
+  if mainMenu == false and fail == false and timeOut == false then -- countdown starts only in-game
     time = time - dt --countdown
 
     if time <= 0 then -- if player runs out of time
+      timeOut = true
       if hiScore < score then -- Test if high score is beaten
         hiScore = score
       end
-      score = 0 --score and timer reset
-      time = timeStart
     end
   end
 
@@ -53,11 +55,10 @@ function love.keypressed(key, scancode, isrepeat)
 
   isrepeat = true
 
-  if mainMenu == false and fail == false then --{
+  if mainMenu == false and fail == false and timeOut == false then --{
     if key == "up" or key == "down" or key == "left" or key == "right" then
       if key == instruction[i] then -- If correct key is pressed
         score = score + 1 --score add
-        punishment = false
       end
 
       if key ~= instruction[i] then -- If wrong key is pressed
@@ -65,9 +66,6 @@ function love.keypressed(key, scancode, isrepeat)
         if hiScore < score then -- Test if high score is beaten
           hiScore = score
           newHiScore = true
-        end
-        if (punishment == true and mainMenu == false) then --punishment for very bad players
-            --love.system.openURL("https://www.youtube.com/embed/DSzlq7SaqTQ?rel=0&amp;autoplay=1;fs=0;autohide=0;hd=0;")
         end
       end
 
@@ -77,7 +75,8 @@ function love.keypressed(key, scancode, isrepeat)
       end
 
       if score >= 30  then -- Instructions rotate randomly (30)
-        angle = math.random(0, 6)
+        angle = love.math.random(0, 6)
+        --a = math.random(1, 4)
       end
 
       love.graphics.setBackgroundColor(love.math.random(0,200),love.math.random(0,200),love.math.random(0,200))
@@ -93,6 +92,7 @@ function love.keypressed(key, scancode, isrepeat)
     time = timeStart --time reset
     score = 0 -- score reset
     newHiScore = false
+    timeOut = false
     angle = 0
   end
 
@@ -101,7 +101,6 @@ end --love.keypressed end
 function love.draw(dt)
 
   if mainMenu == true then --Player in main menu
-    --love.graphics.draw(mos, 500, 390, 0, 0.2)
     love.graphics.setColor(10, 10, 10, 200) --Swipe & Enter to start logo color
     love.graphics.setNewFont(100) --"Swipe" logo size
     love.graphics.printf("SWIPE", 0, 140, 640, "center")
@@ -111,6 +110,16 @@ function love.draw(dt)
     love.graphics.printf("Coded (with Love) by Maxime Leconte and Yann Gaudemer", 0, 350, 640, "center")
     love.graphics.print("Alpha Build", 500, 10)
   else
+    if timeOut == true then --Player runs out of time
+      love.graphics.setNewFont(100)
+      love.graphics.printf("TIME OUT!", 0, 140, 640, "center")
+      love.graphics.setNewFont(30)
+      love.graphics.printf("Your score : "..score, 0, 300, 640, "center")
+      if newHiScore == true then
+        love.graphics.printf("NEW HIGH SCORE!", 0, 330, 640, "center")
+      end
+      love.graphics.printf("Press ENTER to restart", 0, 390, 640, "center")
+    end
     if fail == true then -- Player makes a mistake
       love.graphics.setColor(255, 255, 255, 255) -- OOPS color
       love.graphics.setNewFont(100) -- OOPS size
@@ -129,9 +138,11 @@ function love.draw(dt)
       love.graphics.print("Score : "..score, 360, 10)
       love.graphics.print("High Score : "..hiScore, 10, 10)
       love.graphics.print("Time left : "..math.floor(time), 360, 40)
-      --love.graphics.rotate(angle)
-      love.graphics.print(instruction[i], posx, posy, angle) -- up down left right
     end
+  end
+
+  if mainMenu == false and fail == false and timeOut == false then
+    love.graphics.print(instruction[i], posx, posy, angle) -- up down left right
   end
 
 end --love.draw end
